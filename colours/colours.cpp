3f55570cb139396f480b69f37feeb4f75a714f12
dstarkey23 <ds207@illinois.edu>
2018-09-01 18:12:04 +0100

    colour project

--- colours/colours.cpp
+++ colours/colours.cpp
@@ -39,13 +39,14 @@ int sum = 0, value = 0;
   
 // read until end-of-file, calculating a running total of all values read    
 
-  int N,M,B;
+  int N,M,B,N2;
   cout << "reading Number of colours, number of columns, number of bricks \n";
   cin >> N >> M >> B;
-  int bricks [B][N],score[B];
+  N2 = 2*N;
+  int bricks [B][N2],score[B];
   int n,i1=0,i2= 0,s=0;
-  cout << "N,M,B ="<< N<<" "<<M<<" "<<B;	
-  cout << "for each of the '<< B<< 'bricks, reading... \n";
+  cout << "\n N,M,B ="<< N<<" "<<M<<" "<<B;	
+  cout << "\n for each of the '<< B<< 'bricks, reading... \n";
   
   
   // read first enter \n symbol
@@ -59,24 +60,24 @@ int sum = 0, value = 0;
   s = 1;
   cin >> value;
   while (cin.get() != '\n'){
-  cout << "value " << value<<" i i1 = "<< i<<" "<< i1 << "\n";
+  //cout << "value " << value<<" i i1 = "<< i<<" "<< i1 << "\n";
   bricks[i][i1] = value;
   cin >> value;
   
   //std::cout << "Sum is: " << sum << std::endl;
   if (i1 % 2 == 0) {
   s = s + value;
-  cout << "scoring i = "<< i<< " i1 == "<<i1<<" value="<<value<<"\n";
+  //cout << "scoring i = "<< i<< " i1 == "<<i1<<" value="<<value<<"\n";
   }
   i1 += 1;
   }
-  cout << "value " << value<<" i i1 = "<< i<<" "<< i1 << "\n";
-  cout << "score " << s << "\n";
+  //cout << "value " << value<<" i i1 = "<< i<<" "<< i1 << "\n";
+  //cout << "score " << s << "\n";
   bricks[i][i1] = value;  
   score[i] = s;
   
   //set unused elements to -1
-  for (i2=i1+1;i2<N-1;i2 = i2 + 1){
+  for (i2=i1+1;i2<N2-1;i2 = i2 + 1){
   bricks[i][i2] = -1;
   }
   
@@ -84,7 +85,8 @@ int sum = 0, value = 0;
   cout << "Brick " << i << "\n";
   i2 = 0;
   
-  for( int i2 = 0; i2 < N-1; i2 = i2 + 1 ) {
+  for( int i2 = 0; i2 < N2-1; i2 = i2 + 1 ) {
+  if (bricks[i][i2] == -1) break;
   cout << bricks[i][i2]<< " ";
   }
   cout << " Score = " << score[i];
@@ -95,16 +97,16 @@ int sum = 0, value = 0;
 /* sort the scores so we know which brick gives the highest score.
 Prioritise these bricks when filling the grid */
 int ibsort[B];
-isort(score, ibsort, B )
+isort(score, ibsort, B );
 
 
  
  
 // Bricks now read into bricks array with scores in score array 
-int iclm,ib, ibnow, bnow,i2,icol,bn,skip_brick,score_save=0;
+int iclm,ib, ibnow,icol,bn,skip_brick,icolsum,score_save=0;
 
 //array to denote whether column has a particular colour in it
-int colfill[N] = {0},colsave[M][N],icolsum=0;
+int colfill[N] = {0},colsave[M][N],bnow[N2];
 
 // array to determine if a brick has already been chosen
 int bprev[B]={0};
@@ -113,25 +115,22 @@ int bprev[B]={0};
 for(iclm=0;iclm<M;iclm=iclm+1){
 icolsum=0;
 
-// reset the column output save array
-for(i2=0;i2<N;i2=i2+1) colsave[i2] = -1
-
 // inner loop over bricks in ascending order from high value down
 for (ib=0;ib<B;ib=ib+1){
 if (bprev[ib] == 1) break;
 
-ibnow = ibsort[ib]
+ibnow = ibsort[ib];
 skip_brick = 0;
 
-for (i2=0;i2<N;i2=i2+1) bnow[i2]  = bricks[ibnow][i2];
+for (i2=0;i2<N2;i2=i2+1) bnow[i2]  = bricks[ibnow][i2];
 
 //identify colours in brick and, if a space is available in a column, assign it else skip to next brick
-for (i2=0;i2<N;i2=i2+2) {
+for (i2=0;i2<N2;i2=i2+2) {
 bn = bnow[i2];
 if (colfill[bn] == 1) {
 skip_brick = 1;
 break;
-if (bnow == -1) break;
+if (bn == -1) break;
 }
 colfill[bn] = 1;
 }
@@ -160,9 +159,9 @@ cout << score_save << "\n";
 for (iclm = 0; iclm < M ; iclm = iclm + 1){
 for (i2=0;i2<N;i2=i2+1){
 if (colsave[iclm][i2] == -1) break;
-cout << colsave[iclm][] << " "
+cout << colsave[iclm][i2] << " ";
 }
-cout << "\n"
+cout << "\n";
 }
 
 } // end program
