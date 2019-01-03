88a024a546cd99cf631d4a57c4ef3f3358d798a6
David Starkey <david@Davids-MacBook-Pro.local>
2019-01-03 10:09:05 +0000

    move fish forecast

--- fish_forecast/assemble.py
+++
@@ -1,6 +0,0 @@
-import glob
-import os
-
-
-os.system('cp -rf /Users/david/projects/vortexa/analytics/prediction_functions ./fish_forecast/')
-os.system('cp /Users/david/projects/vortexa/analytics/arbitrage_flow_study/utils.py ./fish_forecast/__init__.py')
