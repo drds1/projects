4bf6b6d14692df63797e89735c4033d1ee8d506a
dstarkey23 <david.starkey@vortexa.com>
2019-03-09 13:00:57 +0000

    add cron test

--- crontest/print_time.py
+++ crontest/print_time.py
@@ -1,5 +1,16 @@
 import datetime
 import time
+import os
+pwd = os.getcwd()+'/'
+
 
 print('the time is...')
-print(datetime.datetime.now())
+time = datetime.datetime.now()
+print(time)
+
+
+#append to file
+with open(pwd+"time.txt", "a") as myfile:
+    myfile.write(np.str(time)+'\n')
+
+
