29a20425d07b621634dc0c2eca67518c94803a46
dstarkey23 <david.starkey@vortexa.com>
2019-10-01 14:30:04 +0100

    compare XGB Classifier with Random Forrest and Logistic regession for diabetes data

--- XGB_diabetes_study/XGM_model.py
+++ XGB_diabetes_study/XGM_model.py
@@ -4,6 +4,8 @@ from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
 # load data
 dataset = loadtxt('diabetes_sample.csv', delimiter=",")
 # split data into X and y
@@ -16,7 +18,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, r
 
 # fit model no training data (compare logistic regression and XGboost)
 model = {'XGB':XGBClassifier(),
-		'Logreg':LogisticRegression(solver='lbfgs',max_iter=1000)}
+		'Logreg':LogisticRegression(solver='lbfgs',max_iter=1000),
+		'RFC':RandomForestClassifier(n_estimators=1000)}
 for m in model.keys():
 	model[m].fit(X_train, y_train)
 	# make predictions for test data
